ac1492e04b8efba15c29e469a4586d8fb4ec495f
Julian Viechter <jviechter@gmail.com>
2021-02-04 00:15:27 +0100

    Make menu ui system thread local

--- src/menu.rs
+++ src/menu.rs
@@ -16,14 +16,14 @@ impl StateStagePlugin<GameState> for MenuStateStagePlugin {
     }
 }
 
-fn ui_system(
-    mut egui_context: ResMut<EguiContext>,
-    mut state: ResMut<State<GameState>>,
-    mut exit_events: ResMut<Events<AppExit>>,
-) {
+fn ui_system(_world: &mut World, resources: &mut Resources) {
+    let mut egui_context = resources.get_mut::<EguiContext>().unwrap();
     let ctx = &mut egui_context.ctx;
+
     egui::SidePanel::left("side_panel", 150.0).show(ctx, |ui| {
         ui.vertical_centered_justified(|ui| {
+            let mut state = resources.get_mut::<State<GameState>>().unwrap();
+
             if ui.button("Play").clicked {
                 state.set_next(GameState::Play).unwrap();
             }
@@ -37,6 +37,7 @@ fn ui_system(
             egui::Layout::bottom_up(egui::Align::Center).with_cross_justify(true),
             |ui| {
                 if ui.button("Exit").clicked {
+                    let mut exit_events = resources.get_mut::<Events<AppExit>>().unwrap();
                     exit_events.send(AppExit);
                 }
             },
